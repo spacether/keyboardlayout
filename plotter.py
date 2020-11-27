2749fa154f50fa42d63da76910fb0dbb3d33d3bb
Justin Black <justin.a.black@gmail.com>
2020-11-27 12:00:55 -0800

    Adds x and y margin input for keys

--- plotter.py
+++ plotter.py
@@ -24,21 +24,22 @@ class TxtSprite(pygame.sprite.Sprite):
         self,
         x: int,
         y: int,
+        ytop: bool,
         txt = '',
         font=None,
         font_color=None,
     ):
-        # TODO extract text into its own sprite
-        # TODO make KeyGroup into its own class that will make:
-        # - background + text sprites
         super().__init__()
         self.image = font.render(txt, 1, font_color)
 
         txt_width = self.image.get_width()
         txt_height = self.image.get_height()
 
-        xloc = x - txt_width/2
-        yloc = y - txt_height
+        xloc = x
+        if ytop:
+            yloc = y
+        else:
+            yloc = y - txt_height
         self.rect = pygame.Rect(xloc, yloc, txt_width, txt_height)
 
 
@@ -58,9 +59,6 @@ class RectSprite(pygame.sprite.Sprite):
 
 
 class KeyGroup(pygame.sprite.Group):
-    xloc_keyunits = 0.2
-    ylocs_keyunits = [0.35, 0.9]
-
     def __init__(
         self,
         x: int,
@@ -71,6 +69,8 @@ class KeyGroup(pygame.sprite.Group):
         label_pair: typing.Tuple[str],
         font: pygame.font.SysFont,
         font_color: pygame.Color,
+        txt_xmargin: int,
+        txt_ymargin: int,
     ):
         super().__init__()
         bg_sprite = RectSprite(x, y, width, height, color)
@@ -79,9 +79,21 @@ class KeyGroup(pygame.sprite.Group):
         for i, label_txt in enumerate(label_pair):
             if not label_txt:
                 continue
-            xloc = x + width*self.xloc_keyunits
-            yloc = y + height*self.ylocs_keyunits[i]
-            txt_sprite = TxtSprite(xloc, yloc, label_txt, font, font_color)
+            xloc = x + txt_xmargin
+            if i == 0:
+                ytop = True
+                yloc = y + txt_ymargin
+            else:
+                ytop = False
+                yloc = y + height - txt_ymargin
+            txt_sprite = TxtSprite(
+                xloc,
+                yloc,
+                ytop,
+                label_txt,
+                font,
+                font_color
+            )
             self.add(txt_sprite)
 
 
@@ -96,7 +108,8 @@ def plot_keyboard(keyboard_layout: str, screen: pygame.Surface):
     font_size = 15
     font = pygame.font.SysFont('Arial', font_size)
     font_color = key_color.__invert__()
-
+    txt_xmargin = 11
+    txt_ymargin = 8
 
     from keyboard_layouts import qwerty
     layout = qwerty
@@ -110,7 +123,6 @@ def plot_keyboard(keyboard_layout: str, screen: pygame.Surface):
             )
             key_width = letter_key_width * key_xsize_keycoords
             key_height = letter_key_height * key_ysize_keycoords
-            print((label_info, key_width, key_height, key_x))
             key_group = KeyGroup(
                 key_x,
                 row_y,
@@ -119,7 +131,9 @@ def plot_keyboard(keyboard_layout: str, screen: pygame.Surface):
                 key_color,
                 label_info[:2],
                 font,
-                font_color
+                font_color,
+                txt_xmargin,
+                txt_ymargin
             )
             keyboard_group.add(key_group.sprites())
             key_x += key_width + gap

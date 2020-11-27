f22c62bf9aaded859dfd0a44e21d523b388886c2
Justin Black <justin.a.black@gmail.com>
2020-11-27 10:27:12 -0800

    Adds and plots first row of keys, backspace txt misaligned

--- keyboard_layouts/qwerty.py
+++ keyboard_layouts/qwerty.py
@@ -1,9 +1,22 @@
 # tuple of upper txt and lower text
 rows = {
     # 'functions': {},
+    # upper text, lower txt, key_name for key detection
     'numbers': [
-        ('~', '`'),
-        ('!', '1'),
+        ('~', '`', '`'),
+        ('!', '1', '1'),
+        ('@', '2', '2'),
+        ('#', '3', '3'),
+        ('$', '4', '4'),
+        ('%', '5', '5'),
+        ('^', '6', '6'),
+        ('&', '7', '7'),
+        ('*', '8', '8'),
+        ('(', '9', '9'),
+        (')', '0', '0'),
+        ('_', '-', '-'),
+        ('+', '=', '='),
+        ('Backspace', '', 'backspace'),
     ],
     # 'letters_1': {'left tab': 'tab'},
     # 'letters_2': {},
@@ -12,7 +25,19 @@ rows = {
     # 'uparrow': {'up arrow': '▲'},
 }
 row_locations = {'numbers': (0, 0)} # in std key size coordinates? where 1, 1 is 1 letter key wide by 1 letter key tall
-key_sizes = {'tab': (1.5, 1), 'return': (2, 1), 'shift': (2, 1), 'command': (1.3, 1.1), 'tall': (1, 1.2), 'space bar': (6, 1)}
+key_sizes = {
+    'tab': (1.5, 1),
+    'return': (2, 1),
+    'shift': (2, 1),
+    'command': (1.3, 1.1),
+    'tall': (1, 1.2),
+    'space bar': (6, 1),
+    'backspace': (2.5, 1),
+}
 rows_to_key_size = {'spacebar_row': 'tall'}
-keys_to_key_size = {'space bar': 'space bar', 'left command': 'command', 'right command': 'command'}
+keys_to_key_size = {
+    'space bar': 'space bar',
+    'left command': 'command',
+    'right command': 'command'
+}
 # if key size is not defined, 1 key x by 1 key y size is assumed
--- plotter.py
+++ plotter.py
@@ -1,11 +1,13 @@
+import typing
+
 import pygame
 
 def init_pygame(keyboard_layout: str) -> pygame.Surface:
     pygame.init()
     pygame.display.set_caption("{} keyboard layout".format(keyboard_layout))
 
-    width = 240
-    height = 180
+    width = 1100
+    height = 300
     screen = pygame.display.set_mode((width,height))
     screen.fill(pygame.Color('black'))
     return screen
@@ -17,7 +19,45 @@ def run_until_window_closed():
             if event.type == pygame.QUIT:
                 running = False
 
-class SpriteRect(pygame.sprite.Sprite):
+class TxtSprite(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        txt = '',
+        font=None,
+        font_color=None,
+    ):
+        # TODO extract text into its own sprite
+        # TODO make KeyGroup into its own class that will make:
+        # - background + text sprites
+        super().__init__()
+        self.image = font.render(txt, 1, font_color)
+
+        txt_width = self.image.get_width()
+        txt_height = self.image.get_height()
+
+        xloc = x - txt_width/2
+        yloc = y - txt_height
+        self.rect = pygame.Rect(xloc, yloc, txt_width, txt_height)
+
+
+class RectSprite(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        color: pygame.Color,
+    ):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = pygame.Rect(x, y, width, height)
+
+
+class KeyGroup(pygame.sprite.Group):
     xloc_keyunits = 0.2
     ylocs_keyunits = [0.35, 0.9]
 
@@ -28,37 +68,28 @@ class SpriteRect(pygame.sprite.Sprite):
         width: int,
         height: int,
         color: pygame.Color,
-        label_pair = (),
-        font=None,
-        font_color=None,
+        label_pair: typing.Tuple[str],
+        font: pygame.font.SysFont,
+        font_color: pygame.Color,
     ):
-        # TODO extract text into its own sprite
-        # TODO make KeyGroup into its own class that will make:
-        # - background + text sprites
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-
-        self.rect = pygame.Rect(x, y, width, height)
+        bg_sprite = RectSprite(x, y, width, height, color)
+        self.add(bg_sprite)
 
         for i, label_txt in enumerate(label_pair):
-            self.textSurf = font.render(label_txt, 1, font_color)
-            txt_width = self.textSurf.get_width()
-            txt_height = self.textSurf.get_height()
-            self.image.blit(
-                self.textSurf,
-                [
-                    width*self.xloc_keyunits - txt_width/2,
-                    # height*self.ylocs_keyunits[i]
-                    height*self.ylocs_keyunits[i] - txt_height
-                ]
-            )
+            if not label_txt:
+                continue
+            xloc = x + width*self.xloc_keyunits
+            yloc = y + height*self.ylocs_keyunits[i]
+            txt_sprite = TxtSprite(xloc, yloc, label_txt, font, font_color)
+            self.add(txt_sprite)
+
 
 def plot_keyboard(keyboard_layout: str, screen: pygame.Surface):
     keyboard_group = pygame.sprite.Group()
 
-    key_width = 60
-    key_height = 60
+    letter_key_width = 60
+    letter_key_height = 60
     gap = 10
     key_color = pygame.Color('grey')
 
@@ -71,19 +102,27 @@ def plot_keyboard(keyboard_layout: str, screen: pygame.Surface):
     layout = qwerty
     for row_name, row_keys in layout.rows.items():
         row_x, row_y = layout.row_locations[row_name]
-        for i, label_pair in enumerate(row_keys):
-            key_x = row_x + i*key_width + i*gap
-            key_rect = SpriteRect(
+        key_x = row_x
+        for i, label_info in enumerate(row_keys):
+            key_name = label_info[-1]
+            key_xsize_keycoords, key_ysize_keycoords = (
+                layout.key_sizes.get(key_name, (1, 1))
+            )
+            key_width = letter_key_width * key_xsize_keycoords
+            key_height = letter_key_height * key_ysize_keycoords
+            print((label_info, key_width, key_height, key_x))
+            key_group = KeyGroup(
                 key_x,
                 row_y,
                 key_width,
                 key_height,
                 key_color,
-                label_pair,
+                label_info[:2],
                 font,
                 font_color
             )
-            keyboard_group.add(key_rect)
+            keyboard_group.add(key_group.sprites())
+            key_x += key_width + gap
 
     keyboard_group.draw(screen)
     pygame.display.update()

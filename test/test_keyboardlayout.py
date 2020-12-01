963b03355c9e9eac543bd2061d7a880f8aef5367
Justin Black <justin.a.black@gmail.com>
2020-12-01 08:38:11 -0800

    Adds test_layout_num_sprites

--- test/test_keyboardlayout.py
+++ test/test_keyboardlayout.py
@@ -4,55 +4,48 @@ import typing
 import pygame
 import keyboardlayout
 
-def get_keyboard(
-    layout_name: str,
-    position: typing.List[int]
-) -> keyboardlayout.KeyboardLayout:
-
-    key_size = 60
-    grey = pygame.Color('grey')
-    key_info = keyboardlayout.KeyInfo(
-        size=(key_size, key_size),  # width, height
-        margin=10,
-        color=grey,
-        txt_color=~grey,  # invert grey
-        txt_font=pygame.font.SysFont('Arial', key_size//4),
-        txt_padding=(key_size//10, key_size//10),
-    )
-    keyboard_info = keyboardlayout.KeyboardInfo(
-        position=(0, 0),
-        padding=2,
-        color=~grey
-    )
-    keyboard_layout = keyboardlayout.KeyboardLayout(
-        layout_name,
-        keyboard_info,
-        key_info,
-    )
-    return keyboard_layout
-
 
 class TestKeyboardLayout(unittest.TestCase):
-    @staticmethod
-    def __init_pygame_and_draw_keyboard(layout_name: str):
+    @classmethod
+    def setUpClass(cls):
         pygame.init()
-        pygame.display.set_caption("{} keyboard layout".format(layout_name))
+        key_size = 60
+        grey = pygame.Color('grey')
+        cls.key_info = keyboardlayout.KeyInfo(
+            size=(key_size, key_size),  # width, height
+            margin=10,
+            color=grey,
+            txt_color=~grey,  # invert grey
+            txt_font=pygame.font.SysFont('Arial', key_size//4),
+            txt_padding=(key_size//10, key_size//10),
+        )
+        cls.keyboard_info = keyboardlayout.KeyboardInfo(
+            position=(0, 0),
+            padding=2,
+            color=~grey
+        )
 
-        keyboard = get_keyboard(layout_name, (0, 0))
-
-        screen = pygame.display.set_mode(
-            (keyboard.rect.width, keyboard.rect.height))
-        screen.fill(pygame.Color('black'))
-
-        keyboard.draw(screen)
-        pygame.display.update()
-
-        pygame.image.save(screen, "sample_images/{}.jpg".format(layout_name))
 
     def test_writes_sample_keyboard_layouts_to_images(self):
         layout_names = ['qwerty', 'azerty_laptop']
         for layout_name in layout_names:
-            screen = self.__init_pygame_and_draw_keyboard(layout_name)
+            pygame.display.set_caption(
+                "{} keyboard layout".format(layout_name))
+
+            keyboard = keyboardlayout.KeyboardLayout(
+                layout_name,
+                self.keyboard_info,
+                self.key_info,
+            )
+
+            screen = pygame.display.set_mode(
+                (keyboard.rect.width, keyboard.rect.height))
+            screen.fill(pygame.Color('black'))
+
+            keyboard.draw(screen)
+            pygame.display.update()
+            pygame.image.save(
+                screen, "sample_images/{}.jpg".format(layout_name))
 
     def test_invalid_layout_name_raise_exception(self):
         with self.assertRaisesRegex(
@@ -63,6 +56,22 @@ class TestKeyboardLayout(unittest.TestCase):
                 'invalid_layout',
                 None,
                 None,
+            )
+
+    def test_layout_num_sprites(self):
+        num_sprites_by_layout = {
+            keyboardlayout.LayoutName.QWERTY: 143,
+            keyboardlayout.LayoutName.AZERTY_LAPTOP: 166,
+        }
+        for layout_name in keyboardlayout.LayoutName:
+            keyboard = keyboardlayout.KeyboardLayout(
+                layout_name,
+                self.keyboard_info,
+                self.key_info,
+            )
+            self.assertEqual(
+                len(keyboard.sprites()),
+                num_sprites_by_layout[layout_name]
             )
 
 
